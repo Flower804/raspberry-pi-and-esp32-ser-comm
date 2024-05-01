3d80f3e15b6a9618988f8442601b4ca25cc16c6d
Flower804 <146494346+Flower804@users.noreply.github.com>
2024-05-01 10:29:59 +0100

    added final python code

--- rasp_ser_beta.py
+++ rasp_ser_beta.py
@@ -3,63 +3,51 @@ import os
 import time
 from datetime import datetime
 
+#private
+testing_state = True
 
 #handlers
 force = True
 Turn  = False
 
 
-ser = serial.Serial('COM5', 9600, timeout=5) #using COM5 for testing purposes only
+#ser = serial.Serial('COM5', 9600, timeout=5) #using COM5 for testing purposes on laptop only
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5) #using /dev/ttyACM0- serial comm for rasp usb connection to esp
+conectsec = ser.readline()
 
 def get_time(): #get current time for multi use
     current_time = f'{datetime.now(): %H_%M_%S}'
     return current_time
 
 def info_dump():
-    f.write(get_time() + '\n')
+    f = open(filename, 'a')
+    f.write("-------" + get_time() + "-------" + '\n' + '\n')
+    f.close()
     while(1):
+        f = open(filename, 'a')
         input_str = ser.readline().decode("utf-8").strip()
-        if input_str == "no_m_up":
+        if input_str == "sent_complete":
+            ser.write(b'done\n')
             return 0
         else:
             f.write(input_str + '\n')
+            f.close()
             
-def lill_test(val, Turn):
-    if val == 1:
-        if Turn == True:
-            ser.write(b'on\n')
-        else:
-            ser.write(b'off\n')
-    else:
-        if Turn == True:
-            Turn = False
-        else:
-            Turn = False
 
 filename = ('sensors_data.txt')
-f = open(filename, 'w')
+f = open(filename, 'a')
 
 while(1):
-    #lill_test(2, Turn) all this is in comment because i dont hecking know what i was on last night
     input_str = ser.readline().decode("utf-8").strip()
     print(input_str) #TODO only for debuggin purposes
     if input_str == "ready" or force == True: #force for when comm is stuck or desicronissed
-        lill_test(1, Turn) #TODO sub for turn or smth
         while(True):
             ser.write(b'on\n') #testing purposes
             input_str = ser.readline().decode("utf-8").strip()
             if(input_str == "received"):
-                break 
-            #input_str = ser.readline().decode("utf-8").strip()
-            #lill_test(1, Turn)
-            ##if input_str == "update_r": #TODO don't forget to take this out and just imediatly start info_dump()
-            #    force = False
-            #    info_dump()
-            #    if info_dump == 0:
-            #        break
-            #elif input_str == "received":
-            #    print("it's working :D")
-            #    force = False
-            #    break
+                info_dump()
+                break
     elif(input_str == "."):
         force = True
+    elif(input_str == "sent_complete"):
+        ser.write(b'done\n')
